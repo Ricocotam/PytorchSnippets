f87d89c1583cde9b39f9f963d46c6d6fa9488b9b
Adrien Pouyet <adrien@pouyet.fr>
2018-11-18 18:13:00 +0100

    utils becomes a package

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .auto_encoders import AutoEncoder, AutoEncoderConv, AutoEncoderFC
+from .utils import fc_block, conv_block, conv_architecture
+from .basics import ConvNet, FullyConnected, FullyConvolutionnal
